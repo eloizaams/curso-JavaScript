1b1e2b165525cf587093b2fc7b7876e1b6b31c9c
Eloiza <eloizaams@yahoo.com.br>
2024-06-29 08:02:46 -0300

    tupos de dados e operadores

---
+++ 02_OPERADORES/js/script.js
@@ -0,0 +1,77 @@
+// // 1 - Number
+// console.log(typeof 2);
+// console.log(typeof 5.14);
+// console.log(typeof -127);
+
+// // 2 - Operações aritméticas
+// console.log(2+4)
+// console.log(10-5)
+// console.log(3*4)
+// console.log(8/2)
+
+// // 3 - Special Numbers
+// console.log(typeof Infinity)
+// console.log(typeof -Infinity)
+// console.log("abc" * 2)
+// console.log(typeof NaN)
+
+// // 4 - String
+// console.log("teste")
+// console.log('mais um texto')
+// console.log('13')
+
+// console.log(typeof "teste")
+// console.log(typeof 'mais um texto')
+// console.log(typeof '13')
+
+// // 5- Símbolos especiais em string
+// console.log("Texto em \nduas linhas")
+
+// console.log("Espaçamento \t de tab")
+
+// // 6 - Concatenação
+// console.log("Meu" + ' ' + "texto" + ' ' + "combinado")
+
+// // 7 - Template strings
+// console.log(`A soma  de 2 + 2 é: ${2+2}`)
+
+// console.log(`Podemos executar qualquer coisa aqui: ${console.log("teste")}`)
+
+// // 8 - Boolean
+// console.log(true)
+// console.log(30<10)
+// console.log(10<=20)
+// console.log(typeof false)
+
+// // 9 - Comparações
+// console.log(5 <= 5)
+// console.log(5 < 5)
+// console.log(5 == 5)
+// console.log(5 == 4)
+// console.log(5 != 4)
+
+// // 10 - Idêntico
+// console.log("9" == 9)
+// console.log("9" === 9)
+// console.log("9" != 9)
+// console.log("9" !== 9)
+// console.log("9" + 9)
+
+// 11 - Operadores lógicos
+console.log(false && true)
+console.log(false || true)
+console.log(!false && true)
+
+// 12 - Empty values
+console.log(typeof null, typeof undefined)
+console.log(null === undefined)
+console.log(null == undefined)
+console.log(null == false)
+console.log(undefined == false)
+
+// 13 - Mudança de tipos
+console.log(5 * null)
+console.log("10"+1)
+console.log("10"-1)
+console.log("test"*"opa")
+
